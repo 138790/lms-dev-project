3f0cc64bddfcdf96636c1724eb0774815cdb77d1
huyong <h18397809987@163.com>
2019-12-31 16:20:33 +0800

    第一次提交

---
+++ src/main/java/com/example/lmsdevproject/LmsDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lmsdevproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsDevProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsDevProjectApplication.class, args);
+    }
+
+}
